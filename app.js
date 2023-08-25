b554cb26786e4d10c52729a93b0aec3dd31b8fc1
Ashutosh Patil <patilashutosh67@gmail.com>
2023-08-25 22:39:22 +0530

    Cleaned the code, updated the readme, project complete!

--- app.js
+++ app.js
@@ -3,51 +3,59 @@ let gridValue = document.querySelector('.grid-size');
 let gridSize = document.querySelector('input');
 const resetBtn = document.querySelector('.reset');
 const applyGridSize = document.querySelector('.apply');
+
+
+//default size
 let squareSize = 8;
+
+//default view of 8 x 8
 
 createGrid(squareSize);
 
-// Create Squared Divs
-function createDiv(size) {
-  const div = document.createElement('div');
-  div.classList.add('box');
-  div.style.width = `${size}px`;
-  div.style.height = `${size}px`;
+//individual div
+function createDiv(size){
+    const div= document.createElement('div');
+    div.classList.add('box');
+    div.style.width=`${size}px`;
+    div.style.height=`${size}px`;
 
-  return div;
+    return div;
 }
 
-// Creat The Grid and append it to grid
-function createGrid(gridSize) {
-  for (let i = 0; i < gridSize; i++) {
-    for (let j = 0; j < gridSize; j++) {
-      grid.appendChild(createDiv(grid.clientWidth / gridSize));
+//create grid
+function createGrid(gridSize){
+    for(i=0;i<gridSize;i++){
+        for(j=0;j<gridSize;j++){
+            grid.appendChild(createDiv(grid.clientWidth/gridSize))
+        }
     }
-  }
 }
 
-function reset() {
-  while (grid.firstChild) {
-    grid.removeChild(grid.lastChild);
-  }
-  createGrid(squareSize);
+//reset
+
+function reset(){
+    while(grid.firstChild){
+        grid.removeChild(grid.lastChild)
+    }
+    createGrid(squareSize)
 }
 
-// Used event delegation to target children of the grid
-grid.addEventListener('mouseover', function (e) {
-  // Add the "active" class to only divs with a "box" class
-  if (e.target.matches('.box')) {
-    e.target.classList.add('active');
-  }
+//event listeners
+
+
+grid.addEventListener('mouseover',(e)=>{
+    if(e.target.matches('.box')){
+        e.target.classList.add('active');
+    }
 });
 
-gridSize.addEventListener('input', function (e) {
-  squareSize = e.target.value;
-  gridValue.textContent = `${squareSize}x${squareSize}`;
+gridSize.addEventListener('input', (e)=>{
+    squareSize= e.target.value;
+    gridValue.textContent=`${squareSize}x${squareSize}`;
 });
 
-applyGridSize.addEventListener('click', function () {
-  reset();
+applyGridSize.addEventListener('click', ()=>{
+    reset();
 });
 
-resetBtn.addEventListener('click', reset);
+resetBtn.addEventListener('click',reset);
